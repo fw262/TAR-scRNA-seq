9a582bf0b34dd12a18170b81a131f5b1f5ac2666
fw262 <56937181+fw262@users.noreply.github.com>
2019-12-06 22:34:43 -0500

    Add files via upload

--- scripts/SingleCellHMM_2.bash
+++ scripts/SingleCellHMM_2.bash
@@ -3,11 +3,13 @@
 INPUT_BAM=$1 #pbmc4k_possorted_genome_bam.bam
 CORE=$2
 MINCOV=$3
-PL=$4
+MERGEBP=$4
+PL=$5
 
-${MINCOV:=5}
 ${CORE:=5}
-${PL:=/workdir/sc2457/SingleCellHMM/}
+${MINCOV:=5}
+${MERGEBP:=500}
+${PL:=./scripts}
 
 PREFIX=`echo ${INPUT_BAM} | rev | cut -d / -f 1 |cut -d . -f 2- |rev`
 tmp="HMM_features"
@@ -20,6 +22,7 @@ echo "Path to SingleCellHMM.R   $PL"
 echo "INPUT_BAM                 $INPUT_BAM"
 echo "temp folder               $TMPDIR"
 echo "number Of thread          $CORE"
+echo "minimum coverage		$MINCOV"
 echo ""
 echo "Reads spanning over splicing junction will join HMM blocks"
 echo "To avoid that, split reads into small blocks before input to groHMM"
@@ -55,14 +58,14 @@ wait
 
 
 echo ""
-echo "Merging HMM blocks within 500bp..."
+echo "Merging HMM blocks within ${MERGEBP}bp..."
 for f in chr*_HMM.bed
 do	
   LC_ALL=C sort -k1,1V -k2,2n --parallel=30 ${f} > ${f}.sorted.bed
   cat ${f}.sorted.bed | grep + > ${f}_plus
   cat ${f}.sorted.bed | grep - > ${f}_minus
-  bedtools merge -s -d 500 -i ${f}_plus > ${f}_plus_merge500 &
-  bedtools merge -s -d 500 -i ${f}_minus > ${f}_minus_merge500 &
+  bedtools merge -s -d ${MERGEBP} -i ${f}_plus > ${f}_plus_merge${MERGEBP} &
+  bedtools merge -s -d ${MERGEBP} -i ${f}_minus > ${f}_minus_merge${MERGEBP} &
   wait_a_second
 done
 
@@ -72,45 +75,45 @@ wait
 #f=${PREFIX}_split.sorted_HMM
 #gzip ${f}.bed &
 
-cat chr*_HMM.bed_plus_merge500 | awk 'BEGIN{OFS="\t"} {print $0, ".", ".", "+"}' > ${PREFIX}_merge500
-cat chr*_HMM.bed_minus_merge500 | awk 'BEGIN{OFS="\t"} {print $0, ".", ".", "-"}' >> ${PREFIX}_merge500
+cat chr*_HMM.bed_plus_merge${MERGEBP} | awk 'BEGIN{OFS="\t"} {print $0, ".", ".", "+"}' > ${PREFIX}_merge${MERGEBP}
+cat chr*_HMM.bed_minus_merge${MERGEBP} | awk 'BEGIN{OFS="\t"} {print $0, ".", ".", "-"}' >> ${PREFIX}_merge${MERGEBP}
 
 mkdir toremove
 for f in chr*_HMM.bed
 do	
-mv ${f}.sorted.bed ${f}_plus ${f}_minus ${f}_plus_merge500 ${f}_minus_merge500 toremove/.
+mv ${f}.sorted.bed ${f}_plus ${f}_minus ${f}_plus_merge${MERGEBP} ${f}_minus_merge${MERGEBP} toremove/.
 done
 
 
 echo ""
 echo "Calculating the coverage..." 
 f=${PREFIX}
-LC_ALL=C sort -k1,1V -k2,2n ${f}_merge500 --parallel=30 > ${f}_merge500.sorted.bed
-rm ${f}_merge500
+LC_ALL=C sort -k1,1V -k2,2n ${f}_merge${MERGEBP} --parallel=30 > ${f}_merge${MERGEBP}.sorted.bed
+rm ${f}_merge${MERGEBP}
 
 
-bedtools coverage -a ${f}_merge500.sorted.bed -b <(zcat ${PREFIX}_split.sorted.bed.gz) -s -counts -split -sorted > ${f}_merge500.sorted.bed_count
+bedtools coverage -a ${f}_merge${MERGEBP}.sorted.bed -b <(zcat ${PREFIX}_split.sorted.bed.gz) -s -counts -split -sorted > ${f}_merge${MERGEBP}.sorted.bed_count
 
 echo ""
 echo "Filtering the HMM blocks by coverage..." 
-#cat ${f}_merge500.sorted.bed_count | awk 'BEGIN{OFS="\t"} ($7 >= 2){print $1, $2, $3, $4, $5, $6}' | gzip > ${f}_merge500_2reads.bed.gz
-cat ${f}_merge500.sorted.bed_count | awk 'BEGIN{OFS="\t"} ($7 >= $MINCOV){print $1, $2, $3, $4, $5, $6}' | gzip > ${f}_merge500_5reads.bed.gz
+#cat ${f}_merge${MERGEBP}.sorted.bed_count | awk 'BEGIN{OFS="\t"} ($7 >= 2){print $1, $2, $3, $4, $5, $6}' | gzip > ${f}_merge${MERGEBP}_2reads.bed.gz
+cat ${f}_merge${MERGEBP}.sorted.bed_count | awk 'BEGIN{OFS="\t"} ($7 >= '$MINCOV'){print $1, $2, $3, $4, $5, $6, $7}' | gzip > ${f}_merge${MERGEBP}_${MINCOV}reads.bed.gz
 
 echo "" 
-echo "#### Please examine if major chromosomes are all present in the final PREFIX_merge500_5reads.bed.gz file ####"
-zcat ${f}_merge500_5reads.bed.gz |cut -f 1 |uniq
+echo "#### Please examine if major chromosomes are all present in the final PREFIX_merge${MERGEBP}_${MINCOV}reads.bed.gz file ####"
+zcat ${f}_merge${MERGEBP}_${MINCOV}reads.bed.gz |cut -f 1 |uniq
 
 echo "" 
-echo "Link the final PREFIX_merge500_5reads.bed.gz file to the working directory"
+echo "Link the final PREFIX_merge${MERGEBP}_${MINCOV}reads.bed.gz file to the working directory"
 cd ..
-ln -s ${TMPDIR}/${f}_merge500_5reads.bed.gz .
+ln -s ${TMPDIR}/${f}_merge${MERGEBP}_${MINCOV}reads.bed.gz .
 
 
 echo ""
 echo "Move intermediate files to  ${TMPDIR}/toremove ..." 
 echo ""
 echo "${TMPDIR}/toremove can be deleted if no error message in SingleCellHMM_Run log file and "
-echo "all major chromosomes are present in the final PREFIX_merge500_5reads.bed.gz file"
+echo "all major chromosomes are present in the final PREFIX_merge${MERGEBP}_${MINCOV}reads.bed.gz file"
 echo ""
 echo ""
 
